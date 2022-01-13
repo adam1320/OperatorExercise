2d87311300562036a024ff358f6f7e4bbf1a8fc1
Adam Brannon <adam1320@rocketmail.com>
2022-01-13 13:10:46 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OperatorExerciseConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Create integer variables
+            int a = 17;
+            int b = 4;
+
+            //Create 2 more integer variables
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+                Console.WriteLine($"{a}/{b} is {b} remainder {remainder}");
+
+
+
+
+
+        }
+    }
+}
